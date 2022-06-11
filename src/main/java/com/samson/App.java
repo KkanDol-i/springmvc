33c639dd833f28fcad8f69564d671d145c33f4b4
KkanDol-i <samson2502@gmail.com>
2022-06-11 15:40:53 +0900

    first commit

---
+++ src/main/java/com/samson/App.java
@@ -0,0 +1,13 @@
+package com.samson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
